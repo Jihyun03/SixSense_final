f218cbd10fee26bc0e1af48a590bd0185cd68ca4
wldnj <wldnjs980409@naver.com>
2022-07-18 18:04:47 +0900

    changed_jw

--- exchange_gu/src/MenuBar/Menu.js
+++ exchange_gu/src/MenuBar/Menu.js
@@ -7,9 +7,10 @@ import Series from './Series'
 import Made from './Made'
 import Gesture from '../Gesture/Gesture'
 
-const arr = ["Films","Series","2x9HD", "Getting closer, getting farther away"]
+
+const arr = ["Films", "GCGF", "2x9HD"];
 const pagearr =[
-    <Film/>, <Series/>, <Made/>
+    <Film/>, <Gesture/>, <Made/> 
 ]
 
 export default function Menu() {
@@ -31,7 +32,6 @@ export default function Menu() {
                 <Tab value={0} label={arr[0]}/>
                 <Tab value={1} label={arr[1]}/>
                 <Tab value={2} label={arr[2]}/>
-                {/* <Tab className="FarClose" value={3} label={arr[3]}/> */}
             </Tabs>
             <div>{pagearr[value]}</div>
         </div>
