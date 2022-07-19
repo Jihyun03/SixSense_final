f64bbe18c2348ff83ec39dc1207939250a095f97
JHLee <gh9439@kaist.ac.kr>
2022-07-19 15:51:35 +0900

    floating

---
+++ exchange_gu/src/MenuBar/Imgshow.js
@@ -0,0 +1,55 @@
+import React from "react";
+import style from "styled-components";
+import Fade from "react-reveal/Fade";
+import { flexbox } from "@mui/system";
+import "./Imgshow_style.css"
+
+const IntroBlock = style.div`
+    @media (max-width: 768px) {
+        
+      }
+      h1 {
+        margin-top: 20vh;
+        margin-left: 3vw;
+        font-weight: 900;
+        font-size: 4vw;
+        font-family: "Montserrat", sans-serif;
+        @media (max-width: 768px) {
+          font-size: 30px;
+        }
+      }
+      h3 {
+        font-weight: 200;
+        font-size: 15px;
+      }
+      
+    `;
+
+export default function Imgshow() {
+
+    return(
+        <div>
+            <div className="Titlecut">
+                <Fade bottom>
+                    <IntroBlock>
+                        <img className="titleImage2" src="img/2x9hd_3.png"/> 
+                    </IntroBlock>
+                </Fade>
+            </div>
+            <div className="Fadeout">
+                <Fade bottom>
+                    <IntroBlock>
+                        <img className="titleImage" src="img/2x9hd_1.jpg"/> 
+                    </IntroBlock>
+                </Fade>
+                <Fade right>
+                    <IntroBlock>
+                        <h1 className="TitText">2x9HD만의 감성</h1>
+                    </IntroBlock>
+                </Fade>
+            </div>
+            
+        </div>
+        
+    );
+}
